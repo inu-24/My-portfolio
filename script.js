dc7443885036caea5c08936fc9d514eac45db568
Inuli Hasara Gunasekara <gunasekarainuli@gmail.com>
2026-05-06 11:49:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log("Portfolio Loaded");
+const toggleBtn = document.getElementById("themeToggle"); 
+
+toggleBtn.addEventListener("click", () => {
+    document.body.classList.toggle("dark");
+
+    toggleBtn.textContent =
+        document.body.classList.contains("dark") ? "☀️" : "🌙";
+});
